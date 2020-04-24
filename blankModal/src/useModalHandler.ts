8cc3a040dc7885a8cb566da5678caa0e28db5881
geonwoo.jeong <doscmDev@gmail.com>
2020-04-24 14:00:47 +0900

    refactor: scroll handler

--- blankModal/src/useModalHandler.ts
+++ blankModal/src/useModalHandler.ts
@@ -4,21 +4,43 @@ export const useModalHandler = () => {
   const { useState } = React;
 
   const [isVisible, setIsVisible] = useState<boolean>(false);
+
   const [overflowY, setoverflowY] = useState<string>(
     document.body.style.overflowY
   );
+  const [overflowX, setoverflowX] = useState<string>(
+    document.body.style.overflowX
+  );
+
+  const currentScrollOptions = {
+    overflowX,
+    overflowY,
+  };
+
+  const scrollLockOptions = {
+    overflowX: "hidden",
+    overflowY: "hidden",
+  };
+
+  const handleBodyScroll = (scrollOptions: {
+    overflowX: string;
+    overflowY: string;
+  }) => {
+    document.body.style.overflowX = scrollOptions.overflowX;
+    document.body.style.overflowY = scrollOptions.overflowY;
+  };
 
   const handleModalActive = () => {
     if (!isVisible) {
       setIsVisible(true);
-      document.body.style.overflowY = "hidden";
+      handleBodyScroll(scrollLockOptions);
     }
   };
 
   const handleModalInactive = () => {
     if (isVisible) {
       setIsVisible(false);
-      document.body.style.overflowY = overflowY;
+      handleBodyScroll(currentScrollOptions);
     }
   };
 
